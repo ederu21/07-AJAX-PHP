9a12c17997a8dd501a4df52aa31851b23c1b2d82
user <user@mail.com>
2022-10-30 12:14:23 -0600

    Indentado

--- js/select.js
+++ js/select.js
@@ -1,4 +1,4 @@
-(()=>{
+(() => {
     $(document).ready(() => {
 
         $.ajax({
@@ -19,12 +19,12 @@
 
                 data.alumnos.forEach(alumno => {
                     let content = "";
-                    content += '<tr id="fila'+alumno.id+'">';
-                    content += '<td>'+alumno.id+'</td>';
-                    content += '<td>'+alumno.nombre+'</td>';
-                    content += '<td>'+alumno.estado+'</td>';
-                    content += '<td class="text-center"><a href="actualizar.html?id='+alumno.id+'" class="btn btn-primary"><i class="fa fa-pencil-square-o"></i></a></td>';
-                    content += '<td class="text-center"><a href="" data-nombre="'+alumno.nombre+'" data-id="'+alumno.id+'" class="btn btn-danger botEliminar"><i class="fa fa-trash-o"></i></a></td>';
+                    content += '<tr id="fila' + alumno.id + '">';
+                    content += '<td>' + alumno.id + '</td>';
+                    content += '<td>' + alumno.nombre + '</td>';
+                    content += '<td>' + alumno.estado + '</td>';
+                    content += '<td class="text-center"><a href="actualizar.html?id=' + alumno.id + '" class="btn btn-primary"><i class="fa fa-pencil-square-o"></i></a></td>';
+                    content += '<td class="text-center"><a href="" data-nombre="' + alumno.nombre + '" data-id="' + alumno.id + '" class="btn btn-danger botEliminar"><i class="fa fa-trash-o"></i></a></td>';
                     content += '</tr>';
 
                     $('#tblRegistros').append(content);
@@ -39,46 +39,46 @@
     })
 
     //sintaxis para aplicar a elementos dinamicos creados por js
-    $("body").on("click",".botEliminar",function(e){
+    $("body").on("click", ".botEliminar", function (e) {
         e.preventDefault();
         //Se obtiene el atributo de data-id
-        let id=$(this).data('id');
-        let nombre= $(this).data('nombre');
+        let id = $(this).data('id');
+        let nombre = $(this).data('nombre');
         swal({
             title: "Estas Seguro",
-            text: "De querer borrar a: "+ nombre,
+            text: "De querer borrar a: " + nombre,
             icon: "warning",
             buttons: true,
             dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-                borrarRegistro(id);
-                
-            } else {
-              swal("No se borro el registro");
-            }
-          });        
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    borrarRegistro(id);
+
+                } else {
+                    swal("No se borro el registro");
+                }
+            });
     })
 
-    const borrarRegistro=(id)=>{        
+    const borrarRegistro = (id) => {
         console.log(id);
 
         $.ajax({
             type: 'POST',
-            url: 'php/servicios/post.borraralumno.php?id='+id,
+            url: 'php/servicios/post.borraralumno.php?id=' + id,
             dataType: 'json'
         })
-        .done(function (data) {
+            .done(function (data) {
 
-            console.log("Correcto!");
+                console.log("Correcto!");
 
-            console.log(data); // Se imprime en consola la api
+                console.log(data); // Se imprime en consola la api
 
-            $('#fila'+id).remove();
-            swal("El registro a sido borrado!", {
-                icon: "success",
-              }); 
-        });
+                $('#fila' + id).remove();
+                swal("El registro a sido borrado!", {
+                    icon: "success",
+                });
+            });
     }
 })();
